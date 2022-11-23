8ed07bd95e8d40992462b24adceee40063d2419a
edyta0106 <edyta0106@gmail.com>
2022-11-23 08:58:58 -0700

    Wrote code in employee.test and employee.js.

---
+++ __tests__/employee.test.js
@@ -0,0 +1,29 @@
+const Employee = require("../lib/employee");
+
+describe("Create an Employee", () => {
+  const employee = new Employee("Edyta", 101, "edyta0106@gmail.com");
+
+  it("Testing for Employees name", () => {
+    expect(employee.name).toBe("Edyta");
+  });
+
+  it("Testing for Employee ID as a number", () => {
+    expect(employee.id).toEqual(expect.any(Number));
+  });
+
+  it("Testing for Employee email", () => {
+    expect(employee.email).toBe("edyta0106@gmail.com");
+  });
+
+  it("Testing getName function", () => {
+    expect(employee.getName()).toBe("Edyta");
+  });
+
+  it("Testing getId function", () => {
+    expect(employee.getId()).toEqual(expect.any(Number));
+  });
+
+  it("Testing getEmail function", () => {
+    expect(employee.getEmail()).toBe("edyta0106@gmail.com");
+  });
+});
---
+++ index.js
@@ -0,0 +1,35 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const questions = [
+  {
+    type: "input",
+    message: "What is your name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "Please provide your ID number",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "Please enter your office number",
+    name: "officeNumber",
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What school did you attend?",
+    name: "school",
+  },
+];
